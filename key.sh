66d072cbb9528f24a24af673ba511c2b7d23c523
Denis Zatsarin <dzatsarin@netris.ru>
2020-12-20 18:20:10 +0300

    fix: comment buttons

---
+++ key.sh
@@ -0,0 +1,1 @@
+openssl genrsa 2048 | openssl pkcs8 -topk8 -nocrypt -out key.pem
--- src/app/containers/App.js
+++ src/app/containers/App.js
@@ -1,30 +1,33 @@
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import SliderMonitor from 'remotedev-slider/lib/Slider';
+
 import { liftedDispatch, getReport } from 'remotedev-app/lib/actions';
 import { getActiveInstance } from 'remotedev-app/lib/reducers/instances';
 import styles from 'remotedev-app/lib/styles';
 import enhance from 'remotedev-app/lib/hoc';
 import DevTools from 'remotedev-app/lib/containers/DevTools';
 import Dispatcher from 'remotedev-app/lib/containers/monitors/Dispatcher';
-import MonitorSelector from 'remotedev-app/lib/components/MonitorSelector';
 import Notification from 'remotedev-app/lib/components/Notification';
-import Instances from 'remotedev-app/lib/components/Instances';
 import Button from 'remotedev-app/lib/components/Button';
 import RecordButton from 'remotedev-app/lib/components/buttons/RecordButton';
-import LockButton from 'remotedev-app/lib/components/buttons/LockButton';
 import DispatcherButton from 'remotedev-app/lib/components/buttons/DispatcherButton';
-import SliderButton from 'remotedev-app/lib/components/buttons/SliderButton';
 import ImportButton from 'remotedev-app/lib/components/buttons/ImportButton';
 import ExportButton from 'remotedev-app/lib/components/buttons/ExportButton';
-import PrintButton from 'remotedev-app/lib/components/buttons/PrintButton';
 import SettingsIcon from 'react-icons/lib/md/settings';
+/*
+import BottomIcon from 'react-icons/lib/md/border-bottom';
+import Instances from 'remotedev-app/lib/components/Instances';
+import LockButton from 'remotedev-app/lib/components/buttons/LockButton';
+import PrintButton from 'remotedev-app/lib/components/buttons/PrintButton';
+import SliderButton from 'remotedev-app/lib/components/buttons/SliderButton';
+import MonitorSelector from 'remotedev-app/lib/components/MonitorSelector';
 import LeftIcon from 'react-icons/lib/md/border-left';
 import RightIcon from 'react-icons/lib/md/border-right';
-import BottomIcon from 'react-icons/lib/md/border-bottom';
+
 import RemoteIcon from 'react-icons/lib/go/radio-tower';
 import PersistIcon from 'react-icons/lib/go/pin';
+import SliderMonitor from 'remotedev-slider/lib/Slider'; */
 
 @enhance
 class App extends Component {
@@ -41,8 +44,8 @@ class App extends Component {
 
   render() {
     const {
-      monitor, position, togglePersist,
-      dispatcherIsOpen, sliderIsOpen, options, liftedState
+      monitor, position, /* togglePersist, */
+      dispatcherIsOpen, /* sliderIsOpen, */ options, liftedState
     } = this.props;
     if (!position && (!options || !options.features)) {
       return (
@@ -54,10 +57,10 @@ class App extends Component {
     const features = options.features || {};
     return (
       <div style={styles.container}>
-        <div style={styles.buttonBar}>
+        {/* <div style={styles.buttonBar}>
           <MonitorSelector selected={monitor}/>
           <Instances selected={this.props.selected} />
-        </div>
+        </div> */}
         <DevTools
           monitor={monitor}
           liftedState={liftedState}
@@ -66,7 +69,7 @@ class App extends Component {
           lib={options.lib || options.explicitLib}
         />
         <Notification />
-        {sliderIsOpen && options.connectionId && options.features.jump &&
+        {/* {sliderIsOpen && options.connectionId && options.features.jump &&
           <SliderMonitor
             monitor="SliderMonitor"
             liftedState={liftedState}
@@ -77,62 +80,62 @@ class App extends Component {
             style={{ padding: '15px 5px' }}
             fillColor="rgb(120, 144, 156)"
           />
-        }
+        } */}
         {dispatcherIsOpen && options.connectionId && options.features.dispatch &&
           <Dispatcher options={options} />
         }
         <div style={styles.buttonBar}>
-          {!window.isElectron && position !== '#left' &&
-          <Button
+          {/* !window.isElectron && position !== '#left' &&
+           <Button
             Icon={LeftIcon}
             onClick={() => { this.openWindow('left'); }}
           />
-          }
-          {!window.isElectron && position !== '#right' &&
+           */}
+          {/* !window.isElectron && position !== '#right' &&
           <Button
             Icon={RightIcon}
             onClick={() => { this.openWindow('right'); }}
           />
-          }
-          {!window.isElectron && position !== '#bottom' &&
+           */}
+          {/* !window.isElectron && position !== '#bottom' &&
           <Button
             Icon={BottomIcon}
             onClick={() => { this.openWindow('bottom'); }}
           />
-          }
+           */}
           {features.pause &&
             <RecordButton paused={liftedState.isPaused} />
           }
-          {features.lock &&
+          {/* features.lock &&
             <LockButton locked={liftedState.isLocked} />
-          }
-          {features.persist &&
+           */}
+          {/* features.persist &&
             <Button
               Icon={PersistIcon}
               onClick={togglePersist}
             >Persist</Button>
-          }
+           */}
           {features.dispatch &&
             <DispatcherButton dispatcherIsOpen={dispatcherIsOpen}/>
           }
-          {features.jump &&
+          {/* features.jump &&
             <SliderButton isOpen={sliderIsOpen}/>
-          }
+          */ }
           {features.import &&
             <ImportButton />
           }
           {features.export &&
             <ExportButton />
           }
-          {position && (position !== '#popup' || navigator.userAgent.indexOf('Firefox') !== -1) &&
+          {/* position && (position !== '#popup' || navigator.userAgent.indexOf('Firefox') !== -1) &&
             <PrintButton />
-          }
-          {!window.isElectron &&
+          */ }
+          {/* !window.isElectron &&
           <Button
             Icon={RemoteIcon}
             onClick={() => { this.openWindow('remote'); }}
           >Remote</Button>
-          }
+           */}
           {(chrome.runtime.openOptionsPage || navigator.userAgent.indexOf('Firefox') !== -1) &&
           <Button
             Icon={SettingsIcon}
